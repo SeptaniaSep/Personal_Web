a85efc02ab274a4e9b862d5943aff617fba93ca6
unknown <hafidzsyahn@gmail.com>
2025-02-11 08:30:34 +0700

    update 11 feb 1. use bootstrap for navbar and blog 2.initialize express server

--- helloworld.js
+++ helloworld.js
@@ -1,10 +1,14 @@
 const http = require("node:http");
 
-const hostame = "127.0.0.1";
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.creatServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hallo Teman!\n");
+  res.end("Hello Friends!\n");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`server berjalan di port ${port}`);
 });
